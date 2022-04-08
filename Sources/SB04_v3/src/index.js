ca35c45d0058be78a4132b24e4bdeb6233328962
superdeveloper718 <96727045+superdeveloper718@users.noreply.github.com>
2022-04-08 01:25:15 -0800

    d

--- Sources/SB04_v3/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-import BaseShot from "./components/BaseShot";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <BaseShot />,
-  rootElement
-);
