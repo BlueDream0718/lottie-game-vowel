d1fc3cc806fd2e757b9d48fa57003fa83b9764d5
superdeveloper718 <103200988+topdevmaster@users.noreply.github.com>
2022-04-23 09:51:30 -0800

    P1

--- src/Scenes/DrawingScene.js
+++ src/Scenes/DrawingScene.js
@@ -1075,7 +1075,7 @@ export default function Scene({ nextFunc, _geo,
                             showingDrawingPanel();
                     }}
                     keepLastFrame={true}
-                    speed={0.6}
+                    speed={0.8}
 
                     src={prePathUrl() + 'lottieFiles/main/' + animtionList[letterNum].path + '.json'}
                     style={{
